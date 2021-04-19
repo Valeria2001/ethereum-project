2da01dbe488f6aa4fb9ce5130b1cf5f6c3e172da
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-04-19 17:05:27 +0300

    Create string2int.sol

---
+++ libs/string2int.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.4.4;
+//взято отсюда -- ethereum.stackexchange.com/questions/10811/solidity-concatenate-uint-into-a-string
+contract TestIntToString {
+
+    string public result;
+
+    function TestIntToString(uint aa, string bb) public returns (string str){
+        // result = uintToString(12345678901234567890);
+        result = appendUintToString(bb, aa);
+        return  result;
+    }
+
+    function uintToString(uint v) constant returns (string str) {
+        uint maxlength = 100;
+        bytes memory reversed = new bytes(maxlength);
+        uint i = 0;
+        while (v != 0) {
+            uint remainder = v % 10;
+            v = v / 10;
+            reversed[i++] = byte(48 + remainder);
+        }
+        bytes memory s = new bytes(i);
+        for (uint j = 0; j < i; j++) {
+            s[j] = reversed[i - 1 - j];
+        }
+        str = string(s);
+    }
+
+    function appendUintToString(string inStr, uint v) constant returns (string str) {
+        uint maxlength = 100;
+        bytes memory reversed = new bytes(maxlength);
+        uint i = 0;
+        while (v != 0) {
+            uint remainder = v % 10;
+            v = v / 10;
+            reversed[i++] = byte(48 + remainder);
+        }
+        bytes memory inStrb = bytes(inStr);
+        bytes memory s = new bytes(inStrb.length + i);
+        uint j;
+        for (j = 0; j < inStrb.length; j++) {
+            s[j] = inStrb[j];
+        }
+        for (j = 0; j < i; j++) {
+            s[j + inStrb.length] = reversed[i - 1 - j];
+        }
+        str = string(s);
+    }
+}
