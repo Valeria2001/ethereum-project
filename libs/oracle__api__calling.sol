5044a492763dd3d43d5f75bc81c21e9ff3151979
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-04-19 19:04:46 +0300

    Update oracle__api__calling.sol

--- libs/oracle__api__calling.sol
+++ libs/oracle__api__calling.sol
@@ -3,13 +3,13 @@ import "https://github.com/provable-things/ethereum-api/blob/master/provableAPI_
 
 contract call_oracle__api is ProvableI {
 
-    function request(string memory _query, string memory _method, string memory _url, string memory _kwargs) public payable {
+    function request(string memory _method, string memory _url, string memory _kwargs) public payable {
       if (provable_getPrice(“computation”) > address(this).balance) {
        emit LogNewProvableQuery(“Provable query was NOT sent, please add some ETH to cover for the query fee”);
       } 
       else {
          emit LogNewProvableQuery(“Provable query was sent, standing by for the answer…”);
-         provable_query(“computation”,[_query,_method,_url,_kwargs]);
+         provable_query(“computation”,[_method,_url,_kwargs]);
       }
     }
  } 
