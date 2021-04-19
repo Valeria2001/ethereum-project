e614c854cf7b88a152873afbaf590e70aa59a843
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-04-19 19:13:44 +0300

    Update erc1155-contract.sol

--- erc1155-contract.sol
+++ erc1155-contract.sol
@@ -1,27 +1,36 @@
-
 pragma solidity ^0.6.0;
+import "https://github.com/Valeria2001/ethereum-project/blob/main/libs/string2int.sol";
+import "https://github.com/Valeria2001/ethereum-project/blob/main/libs/oracle__api__calling.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 
-contract ERC1155Basic is ERC1155 {
+contract ERC1155Basic is ERC1155, TestIntToString, call_oracle__api {
     
     uint256 public COUNTER; // все должно быть приватным
     uint256 public my_contract_number_erc20;
     uint256 public  my_contract_token721_id;
+    string public _baseduri;     // url google cloud хостинга
 
-    constructor() public ERC1155("https://lera.github.hosting/api/item/{id}.json") { //обращение по api(что-то на стороне сервака будет ловить запрос
+    constructor() public ERC1155() { //обращение по api(что-то на стороне сервака будет ловить запрос)
         
+    	_baseduri = "https://lera.github.hosting/api"
         COUNTER = 0;
     }
         
 
-    function create_erc1155_token(number_erc20, token721_id) public { //надо понять как сделать приватной позже    
-         my_contract_number_erc20 = number_erc20;
+    function create_erc1155_token(number_erc20, token721_id) public returns(uint256){ //надо понять как сделать приватной позже    
+        
+        my_contract_number_erc20 = number_erc20;
 	my_contract_token721_id =  token721_id;
-        COUNTER++;    
+        COUNTER++;                                   //id  токена
+ 	_baseduri = TestIntToString(COUNTER, _baseduri)// формируем url    
 	_mint(msg.sender, COUNTER, 1, "");  //в api  нало прописать  создание стандартной json формы
-		   
+
+        //здесь пропишем функцию обращения по api к серверу flask для  создания json
+	call_oracle__api("", "GET", _baseduri, ""); //тут  обращаемся к url адресу   flask дальше  он все делает за нас
+	return COUNTER	   
     }
 
 
        
 }
+
