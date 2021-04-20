7dd115b04c98102ea340c27b4b003efb656f0dc4
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-04-20 17:12:16 +0300

    Update oracle__api__calling.sol
    компилируемая версия

--- libs/oracle__api__calling.sol
+++ libs/oracle__api__calling.sol
@@ -1,18 +1,15 @@
+pragma solidity > 0.5.1 < 0.7.0; 
+import "https://github.com/provable-things/ethereum-api/blob/master/provableAPI_0.6.sol"; //подобрать актуальную  версию
 
-
-pragma solidity >= 0.4.22 < 0.5;
-//pragma solidity ^0.4.4;
-import "https://github.com/provable-things/ethereum-api/blob/master/provableAPI_0.4.25.sol"; //подобрать актуальную  версию
-
-contract call_oracle__api is ProvableI {
+contract call_oracle__api is usingProvable {
 
     function request(string memory _query, string memory _method, string memory _url, string memory _kwargs) public payable { 
-      if (provable_getPrice(“computation”) > address(this).balance) {
-       emit LogNewProvableQuery(“Provable query was NOT sent, please add some ETH to cover for the query fee”);
+      if (provable_getPrice("computation") > address(this).balance) {
+       //emit LogNewProvableQuery("Provable query was NOT sent, please add some ETH to cover for the query fee");
       } 
       else {
-         emit LogNewProvableQuery(“Provable query was sent, standing by for the answer…”);
-         provable_query(“computation”,[_query,_method,_url,_kwargs]);
+         //emit LogNewProvableQuery("Provable query was sent, standing by for the answer");
+         provable_query("computation",[_query,_method,_url,_kwargs]);
       }
     }
- } 
+ }  
