4b798713cb7b6bb5ebc8d9672da1dbc79fe58c1e
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-02 15:29:56 +0300

    Create Simpletoken20.sol

---
+++ Simpletoken20.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SimpleToken20 is ERC20 {
+
+    /**
+     * @dev Constructor that gives _msgSender() all of existing tokens.
+     */
+    constructor () public ERC20() {
+        _mint(_msgSender(), 10000);
+    }
+}
