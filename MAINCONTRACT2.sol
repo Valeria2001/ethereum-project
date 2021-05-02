24dd93c38ff27d515cc00d78d834e84df3ffadea
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-02 10:21:25 +0300

    Update MAINCONTRACT2.sol

--- MAINCONTRACT2.sol
+++ MAINCONTRACT2.sol
@@ -1,3 +1,4 @@
+
 pragma solidity ^0.5.0;
 
 import "./myERC721.sol";
@@ -8,18 +9,22 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 contract tokenFactory {
 
     address[] tokenAddress;
-    //IERC20 token20;  обьявляем структуру токена erc20
+    IERC20 token20;  //обьявляем структуру токена erc20
 
    constructor(IERC20 token1)  public {
 
-    IERC20 token20; 
+    //IERC20 token20; 
+    token20 = token1;
 
    }
- 
+
+    function transferErc20() public payable { //переправляем токены 
+
+      token20.transferFrom(msg.sender, address(this), 1);
+
+    }
     function deploy721Contract(string calldata name,string calldata symbol,string calldata baseUrl) external returns (myERC721 cardAddress) { //сюда передаются параметры для токена из веба
-        
-        token20.transferFrom(msg.sender, address(this), 1);
-        token20.transferFrom(msg.sender, address(this), 1);// тут должны быть  id erc20  на основе чего генерится  все
+       
         myERC721 newCards = new myERC721(name, symbol, baseUrl);
 
         tokenAddress.push(address(newCards));  //надо  дбавить  erc1155 factory
