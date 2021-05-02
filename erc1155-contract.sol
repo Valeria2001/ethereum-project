6cd7cf5107edf9e2019870bae93f71b3f18cf4c0
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-02 15:15:15 +0300

    Update erc1155-contract.sol

--- erc1155-contract.sol
+++ erc1155-contract.sol
@@ -1,32 +1,34 @@
 pragma solidity >=0.6.0 <=0.8.3;
-import "https://github.com/Valeria2001/ethereum-project/blob/main/libs/string2int.sol";
-import "https://github.com/Valeria2001/ethereum-project/blob/main/libs/oracle__api__calling.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/token/ERC1155/ERC1155.sol";
+import "./string2int.sol";
+//import "https://github.com/Valeria2001/ethereum-project/blob/main/libs/oracle__api__calling.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract ERC1155Basic is ERC1155, TestIntToString, call_oracle__api {
-    
+//contract ERC1155Basic is ERC1155, TestIntToString, call_oracle__api {
+
+
+contract ERC1155Basic is ERC1155, TestIntToString {  
     uint256 public COUNTER; // все должно быть приватным
     uint256 public my_contract_number_erc20;
-    uint256 public  my_contract_token721_id;
+    uint256 public my_contract_token721_id;
     string public _baseduri;     // url google cloud хостинга
 
-    constructor() public ERC1155("token"){ //обращение по api(что-то на стороне сервака будет ловить запрос)
+    constructor() public ERC1155(""){ //обращение по api(что-то на стороне сервака будет ловить запрос)
     	_baseduri = "https://lera.github.hosting/api";
         COUNTER = 0;
     }
         
 
-    function create_erc1155_token(uint256 number_erc20,uint256  token721_id) public returns(uint256){ //надо понять как сделать приватной позже    
-        
-        my_contract_number_erc20 = number_erc20;
-	    my_contract_token721_id =  token721_id;
-        COUNTER++;                                   //id  токена
- 	    _baseduri = ITestIntToString(COUNTER, _baseduri);// формируем url    
-	    _mint(msg.sender, COUNTER, 1, "");  //в api  нало прописать  создание стандартной json формы
+    function create_erc1155_token(uint256 number_erc20, uint256 token721_id) external returns(uint256){ //надо понять как сделать приватной позже    
+       address ad_con;
+       my_contract_number_erc20 = number_erc20;
+       my_contract_token721_id =  token721_id;
+       COUNTER++;                                   //id  токена
+       _baseduri = ITestIntToString(COUNTER, _baseduri);// формируем url    
+       _mint(msg.sender, 1, 1, "aaa");  //в api  нало прописать  создание стандартной json формы
 
-        //здесь пропишем функцию обращения по api к серверу flask для  создания json
-     	request("aaa", "GET", _baseduri, "aaa"); //тут  обращаемся к url адресу   flask дальше  он все делает за нас
-    	return COUNTER;	   
+       //здесь пропишем функцию обращения по api к серверу flask для  создания json
+       //request("aaa", "GET", _baseduri, "aaa"); //тут  обращаемся к url адресу   flask дальше  он все делает за нас
+       return COUNTER;	   
     }
        
 }
