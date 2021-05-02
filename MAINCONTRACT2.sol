c553e510b17b6ce4fe05b84cc14fe89ecd6d9480
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-02 15:26:19 +0300

    Update MAINCONTRACT2.sol

--- MAINCONTRACT2.sol
+++ MAINCONTRACT2.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.5.0;
 
 import "./myERC721.sol";
@@ -25,6 +24,8 @@ contract tokenFactory {
     }
     function deploy721Contract(string calldata name,string calldata symbol,string calldata baseUrl) external returns (myERC721 cardAddress) { //сюда передаются параметры для токена из веба
        
+        
+        require(token20.balanceOf(address(this))>= 2, "You need to send some erc20");//error но поебать
         myERC721 newCards = new myERC721(name, symbol, baseUrl);
 
         tokenAddress.push(address(newCards));  //надо  дбавить  erc1155 factory
