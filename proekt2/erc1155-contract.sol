1b99f5cf0a7330a16d74ef0a2607d13e747c0f1c
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-07 11:24:31 +0300

    Update erc1155-contract.sol

--- proekt2/erc1155-contract.sol
+++ proekt2/erc1155-contract.sol
@@ -6,30 +6,26 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 //contract ERC1155Basic is ERC1155, TestIntToString, call_oracle__api {
 
 
-contract ERC1155Basic is ERC1155 {  
+contract ERC1155Basic is ERC1155 {
     uint256 public COUNTER; // все должно быть приватным
     uint256 public my_contract_number_erc20;
     uint256 public my_contract_token721_id;
     string public _baseduri;     // url google cloud хостинга
 
     constructor() public ERC1155(""){ //обращение по api(что-то на стороне сервака будет ловить запрос)
-    	_baseduri = "https://lera.github.hosting/api";
+        _baseduri = "https://lera.github.hosting/api";
         COUNTER = 0;
     }
-        
 
-    function create_erc1155_token(uint256 number_erc20, uint256 token721_id) external returns(uint256){ //надо понять как сделать приватной позже    
-       address ad_con;
-       my_contract_number_erc20 = number_erc20;
-       my_contract_token721_id =  token721_id;
-       COUNTER++;                                   //id  токена  
-       _mint(msg.sender, COUNTER, 1, "aaa");  
-       //здесь пропишем функцию обращения по api к серверу flask для  создания json
-       //request("aaa", "GET", _baseduri, "aaa"); //тут  обращаемся к url адресу   flask дальше  он все делает за нас
-       return COUNTER;	   
+
+    function create_erc1155_token(bytes calldata token_data) external returns(uint256){ //надо понять как сделать приватной позже
+       COUNTER++;
+       _mint(msg.sender, COUNTER, 1, token_data);
+       return COUNTER;
     }
-       
+
 }
+
 
 
    
