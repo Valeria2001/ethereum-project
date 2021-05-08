315f2ceefab7fb7e53262354da8c80a577f0906f
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-08 09:28:34 +0300

    Update erc1155-contract.sol

--- proekt2/erc1155-contract.sol
+++ proekt2/erc1155-contract.sol
@@ -1,19 +1,15 @@
 pragma solidity >=0.6.0 <=0.8.3;
-//import "./string2int.sol";
-//import "https://github.com/Valeria2001/ethereum-project/blob/main/libs/oracle__api__calling.sol";
+
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-//contract ERC1155Basic is ERC1155, TestIntToString, call_oracle__api {
 
 
 contract ERC1155Basic is ERC1155 {
     uint256 public COUNTER; // все должно быть приватным
     uint256 public my_contract_number_erc20;
     uint256 public my_contract_token721_id;
-    string public _baseduri;     // url google cloud хостинга
-
-    constructor() public ERC1155(""){ //обращение по api(что-то на стороне сервака будет ловить запрос)
-        _baseduri = "https://lera.github.hosting/api";
+    string public _baseduri;     
+    constructor() public ERC1155(""){ 
         COUNTER = 0;
     }
 
