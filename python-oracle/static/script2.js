5643895a255c5e3fa8d6974b4ec715ebbf81d44e
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-10 20:08:11 +0300

    Update script2.js

--- python-oracle/static/script2.js
+++ python-oracle/static/script2.js
@@ -1,7 +1,39 @@
- if (typeof web3 !== 'undefined') {
-web3 = new Web3(web3.currentProvider);
-} else {
-// set the provider you want from Web3.providers
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+h1 {
+    border: 2px #eee solid;
+    color: brown;
+    text-align: center;
+    padding: 10px;
 }
-
+h2 {
+    border: 2px #eee solid;
+    color: blue;
+    text-align: center;
+    padding: 10px;
+}
+h3 {
+    border: 2px #eee solid;
+    color: green;
+    text-align: center;
+    padding: 10px;
+}
+h4 {
+    border: 2px #eee solid;
+    color: black;
+    text-align: center;
+    padding: 10px;
+    font-size: 150%
+}
+h5 {
+    border: 2px #eee solid;
+    color: black;
+    text-align: center;
+    padding: 10px;
+    font-size: 150%
+}
+h6 {
+    border: 2px #eee solid;
+    color: black;
+    text-align: center;
+    padding: 10px;
+    font-size: 150%
+}
