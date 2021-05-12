64a49d7b825efbfaec990d2b1e14f1bb6f000fc0
Valeria2001 <59765280+Valeria2001@users.noreply.github.com>
2021-05-12 16:43:28 +0300

    Update erc1155-contract.sol

--- proekt2/erc1155-contract.sol
+++ proekt2/erc1155-contract.sol
@@ -1,11 +1,11 @@
+//WARNING! Do not use in main ethereum network
+//This code minting erc1155 token with sample data in it
 pragma solidity >=0.6.0 <=0.8.3;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-
-
 contract ERC1155Basic is ERC1155 {
-    uint256 public COUNTER; // все должно быть приватным
+    uint256 public COUNTER; 
     uint256 public my_contract_number_erc20;
     uint256 public my_contract_token721_id;
     string public _baseduri;     
@@ -14,7 +14,7 @@ contract ERC1155Basic is ERC1155 {
     }
 
 
-    function create_erc1155_token(bytes calldata token_data) external returns(uint256){ //надо понять как сделать приватной позже
+    function create_erc1155_token(bytes calldata token_data) external returns(uint256){ 
        COUNTER++;
        _mint(msg.sender, COUNTER, 1, token_data);
        return COUNTER;
