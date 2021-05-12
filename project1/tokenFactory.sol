0ba655b6174ca32bc4dfbf79e9dce96ed8306fe5
v0landem0rt <59765280+v0landem0rt@users.noreply.github.com>
2021-05-12 16:58:29 +0300

    Update tokenFactory.sol

--- project1/tokenFactory.sol
+++ project1/tokenFactory.sol
@@ -8,11 +8,10 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 contract tokenFactory {
 
     address[] tokenAddress;
-    IERC20 token20;  //обьявляем структуру токена erc20
+    IERC20 token20;  
 
    constructor(IERC20 token1)  public {
 
-    //IERC20 token20; 
     token20 = token1;
 
    }
@@ -21,11 +20,10 @@ contract tokenFactory {
     function deploy721Contract(string calldata name,string calldata symbol,string calldata baseUrl) external returns (myERC721 cardAddress) { //сюда передаются параметры для токена из веба
        
         
-        //require(token20.balanceOf(address(this))>= 2, "You need to send some erc20");error но поебать
         myERC721 newCards = new myERC721(name, symbol, baseUrl);
 
-        tokenAddress.push(address(newCards));  //надо  дбавить  erc1155 factory
-        return newCards;
+        tokenAddress.push(address(newCards)); 
+        return newCards; //returns msg.sender erc721 token
     }
 
 }
